5af6af7225418f05c47545034ef6c549bb1c34b9
dansamargiu <samargiudan@gmail.com>
2013-11-04 13:01:36 -0500

    Added Injector Test

---
+++ src/WinMemoryMapper/InjectionTest/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectionTest
+{
+    [Flags]
+    public enum ProcessCreationFlags : uint
+    {
+        ZERO_FLAG = 0x00000000,
+        CREATE_BREAKAWAY_FROM_JOB = 0x01000000,
+        CREATE_DEFAULT_ERROR_MODE = 0x04000000,
+        CREATE_NEW_CONSOLE = 0x00000010,
+        CREATE_NEW_PROCESS_GROUP = 0x00000200,
+        CREATE_NO_WINDOW = 0x08000000,
+        CREATE_PROTECTED_PROCESS = 0x00040000,
+        CREATE_PRESERVE_CODE_AUTHZ_LEVEL = 0x02000000,
+        CREATE_SEPARATE_WOW_VDM = 0x00001000,
+        CREATE_SHARED_WOW_VDM = 0x00001000,
+        CREATE_SUSPENDED = 0x00000004,
+        CREATE_UNICODE_ENVIRONMENT = 0x00000400,
+        DEBUG_ONLY_THIS_PROCESS = 0x00000002,
+        DEBUG_PROCESS = 0x00000001,
+        DETACHED_PROCESS = 0x00000008,
+        EXTENDED_STARTUPINFO_PRESENT = 0x00080000,
+        INHERIT_PARENT_AFFINITY = 0x00010000
+    }
+
+    public struct STARTUPINFO
+    {
+        public uint cb;
+        public string lpReserved;
+        public string lpDesktop;
+        public string lpTitle;
+        public uint dwX;
+        public uint dwY;
+        public uint dwXSize;
+        public uint dwYSize;
+        public uint dwXCountChars;
+        public uint dwYCountChars;
+        public uint dwFillAttribute;
+        public uint dwFlags;
+        public short wShowWindow;
+        public short cbReserved2;
+        public IntPtr lpReserved2;
+        public IntPtr hStdInput;
+        public IntPtr hStdOutput;
+        public IntPtr hStdError;
+    }
+
+    public struct PROCESS_INFORMATION
+    {
+        public IntPtr hProcess;
+        public IntPtr hThread;
+        public uint dwProcessId;
+        public uint dwThreadId;
+    }
+
+    [Flags]
+    public enum AllocationType
+    {
+        Commit = 0x1000,
+        Reserve = 0x2000,
+        Decommit = 0x4000,
+        Release = 0x8000,
+        Reset = 0x80000,
+        Physical = 0x400000,
+        TopDown = 0x100000,
+        WriteWatch = 0x200000,
+        LargePages = 0x20000000
+    }
+
+    [Flags]
+    public enum MemoryProtection
+    {
+        Execute = 0x10,
+        ExecuteRead = 0x20,
+        ExecuteReadWrite = 0x40,
+        ExecuteWriteCopy = 0x80,
+        NoAccess = 0x01,
+        ReadOnly = 0x02,
+        ReadWrite = 0x04,
+        WriteCopy = 0x08,
+        GuardModifierflag = 0x100,
+        NoCacheModifierflag = 0x200,
+        WriteCombineModifierflag = 0x400
+    }
+
+    public static class NativeMethods
+    {
+        [DllImport("kernel32.dll")]
+        public static extern bool CreateProcess(string lpApplicationName,
+               string lpCommandLine, IntPtr lpProcessAttributes,
+               IntPtr lpThreadAttributes,
+               bool bInheritHandles, ProcessCreationFlags dwCreationFlags,
+               IntPtr lpEnvironment, string lpCurrentDirectory,
+               ref STARTUPINFO lpStartupInfo,
+               out PROCESS_INFORMATION lpProcessInformation);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint ResumeThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll")]
+        public static extern uint SuspendThread(IntPtr hThread);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, AllocationType flAllocationType, MemoryProtection flProtect);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("kernel32")]
+        public static extern IntPtr CreateRemoteThread(
+          IntPtr hProcess,
+          IntPtr lpThreadAttributes,
+          uint dwStackSize,
+          IntPtr lpStartAddress, // raw Pointer into remote process
+          IntPtr lpParameter,
+          uint dwCreationFlags,
+          out uint lpThreadId
+        );
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        internal static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var processpath = @"D:\dev\WinMemoryMapper\src\WinMemoryMapper\ConsoleApplication1test\bin\Debug\ConsoleApplication1test.exe";
+
+            // Start the process in suspend mode
+            var si = new STARTUPINFO();
+            var pi = new PROCESS_INFORMATION();
+            var success = NativeMethods.CreateProcess(processpath, null,
+                IntPtr.Zero, IntPtr.Zero, false,
+                ProcessCreationFlags.CREATE_SUSPENDED,
+                IntPtr.Zero, null, ref si, out pi);
+
+            string bufferString = @"D:\dev\WinMemoryMapper\src\WinMemoryMapper\WinApiHooks\bin\Debug\WinApiHooks.dll";
+            
+            byte[] buffer = Encoding.Unicode.GetBytes(bufferString.ToCharArray());
+            uint size = (uint)Encoding.Unicode.GetByteCount(bufferString.ToCharArray());
+
+            // allocate memory
+            var argumentPtr = NativeMethods.VirtualAllocEx(pi.hProcess, IntPtr.Zero, size, AllocationType.Commit, MemoryProtection.ReadWrite);
+
+            // write memory
+            var dummy = UIntPtr.Zero;
+            if (!NativeMethods.WriteProcessMemory(pi.hProcess, argumentPtr, buffer, size, out dummy))
+            {
+                // err...
+                Console.WriteLine("WriteProcessMemory false");
+            }
+
+            var krnl32 = NativeMethods.GetModuleHandle("Kernel32");
+            uint dummy2 = 0;
+            var threadPtr = NativeMethods.CreateRemoteThread(pi.hProcess, IntPtr.Zero, 0, NativeMethods.GetProcAddress(krnl32, "GetCursorPos"), argumentPtr, 0, out dummy2);
+
+            var ThreadHandle = pi.hThread;
+            NativeMethods.ResumeThread(ThreadHandle);
+        }
+    }
+}
